7449818404acfe1785cf59be1282dea100cc5c4f
hezhaoyin <2561033753@qq.com>
2018-10-18 10:55:52 +0800

    update
    update

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <!-- <div class="about-content">因为平时还要上班，可能暂时还没太多时间更新这个小程序，但是我会坚持的，大家如果有什么建议，可以点击下面的按钮，提交给我，大家如果有什么素材想要的话，也可以跟我说一下</div> -->
-    <button class="go-btn" type="primary" @click="goSuggestion">提提意见~</button>
-    
+   <button class="go-btn" type="primary" @click="goSuggestion">提提意见~</button>
   </div>
 </template>
 
